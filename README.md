9a1494c2a4602c8b37a27e2ee25db4a353ac73f8
Alistair Hey <alistair@heyal.co.uk>
2020-07-15 08:53:49 +0100

    initial
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

---
+++ README.md
@@ -0,0 +1,22 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/f1a407d3-d323-4282-ae9a-15b07fb6f8d0/deploy-status)](https://app.netlify.com/sites/objective-varahamihira-3d23f6/deploys)
+
+
+## Usage
+
+### Locally
+
+`bundle exec jekyll serve`
+
+### Production
+
+`JEKYLL_ENV=production bundle exec jekyll build`
+
+## Install
+
+
+```shell
+$ bundle install
+$ yarn install
+
+$ yarn run tailwind init _includes/tailwind.config.js
+```
---
+++ js/main.js
@@ -0,0 +1,42 @@
+function initNavToggler() {
+    var navbarOpen = document.querySelector('#open-button');
+    var menu = document.querySelector('#mobile-menu');
+    var navbarClose = document.querySelector('#close-button')
+    var linkClick = document.querySelector('.link-item')
+
+
+    navbarOpen.addEventListener('click', function(evt) {
+        if (menu.classList.contains("hidden")) {
+            menu.classList.remove("hidden")
+        } else {
+            menu.classList.add("hidden")
+        }
+        evt.stopImmediatePropagation()
+    });
+    navbarClose.addEventListener('click', function(evt) {
+        if (menu.classList.contains("hidden")) {
+            menu.classList.remove("hidden")
+        } else {
+            menu.classList.add("hidden")
+        }
+        evt.stopImmediatePropagation()
+    });
+
+    linkClick.addEventListener('click', function(evt) {
+        if (menu.classList.contains("hidden")) {
+            menu.classList.remove("hidden")
+            console.log("show")
+        } else {
+            menu.classList.add("hidden")
+            console.log("hide")
+        }
+        evt.stopImmediatePropagation()
+    });
+}
+
+
+/*------------Init scripts on pageload--------------*/
+/*--------------------------------------------------*/
+document.addEventListener('DOMContentLoaded', function() {
+    initNavToggler();
+})
