0590ed5b2ed5c50aade57c4f6c057dce911a1915
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-12-20 12:25:18 +0000

    Lazy load YouTube video when clicked, add poster
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function initNavToggler() {
     var navbarOpen = document.querySelector('#open-button');
     var menu = document.querySelector('#mobile-menu');
     var navbarClose = document.querySelector('#close-button')
-    var linkClick = document.querySelector('.link-item')
+    // var linkClick = document.querySelector('.link-item')
 
 
     navbarOpen.addEventListener('click', function(evt) {
@@ -22,16 +22,16 @@ function initNavToggler() {
         evt.stopImmediatePropagation()
     });
 
-    linkClick.addEventListener('click', function(evt) {
-        if (menu.classList.contains("hidden")) {
-            menu.classList.remove("hidden")
-            console.log("show")
-        } else {
-            menu.classList.add("hidden")
-            console.log("hide")
-        }
-        evt.stopImmediatePropagation()
-    });
+    // linkClick.addEventListener('click', function(evt) {
+    //     if (menu.classList.contains("hidden")) {
+    //         menu.classList.remove("hidden")
+    //         console.log("show")
+    //     } else {
+    //         menu.classList.add("hidden")
+    //         console.log("hide")
+    //     }
+    //     evt.stopImmediatePropagation()
+    // });
 }
 
 
